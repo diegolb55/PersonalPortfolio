68c2fb6861964885eac6b2da41fb61cce7a81826
diegolb55 <diegolugobendezu@gmail.com>
2023-01-30 14:43:23 -0400

    worked on sizes, colors, positions, flex

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,29 @@
 import styles from "@/styles/Footer.module.css"
 import {BsGithub, BsLinkedin} from "react-icons/bs"
 import { IoTriangleOutline } from "react-icons/io5"
+import { useRef } from "react"
 
 export default function Footer(){
+
+    const goToTop = () => {
+        if( typeof window !== 'undefined'){
+            window.scroll({
+                top: 0,
+                left: 0,
+                behavior: "smooth"
+            })
+        }
+    }
+
     return (
         <div className={styles.footer}>
             <div className={styles.socials}>
                 <BsGithub />
                 <BsLinkedin />
             </div>
-            <div className={styles.arrows}>
-                <IoTriangleOutline className={styles.a1}/>
-                <IoTriangleOutline className={styles.a2}/>
+            <div className={styles.arrows} >
+                <IoTriangleOutline className={styles.a1} onClick={()=>goToTop()}/>
+                <IoTriangleOutline className={styles.a2} onClick={()=>goToTop()}/>
             </div>
         </div>
 
--- components/Skills.js
+++ components/Skills.js
@@ -6,15 +6,15 @@ export default function Skills(){
         <div className={styles.skills}>
             <p>Tech Skills</p>
             <div>
-                <p>Languages</p>
+                <p>Languages:</p>
                 <p>Python, C++, Java, JavaScript, HTML5, CSS, SCSS</p>
             </div>
             <div>
-                <p>Libraries</p>
+                <p>Libraries:</p>
                 <p>Git, GitHub, VS Code, Firebase</p>
             </div>
             <div>
-                <p>Frameworks</p>
+                <p>Frameworks:</p>
                 <p>Next js, Scrum</p>
             </div>
         </div>
